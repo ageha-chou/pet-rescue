79d5e01b20b0b78e6197cd88945276e81d9b81e8
ageha-chou <agehachou2602@gmail.com>
2021-10-25 11:29:24 +0700

    changed profile font style

---
+++ lib/modules/me/user_profile.dart
@@ -0,0 +1,326 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pet_rescue/shared/constants/color.dart';
+
+import 'volunteer_edit_profile.dart';
+
+class UserProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 22),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Image.asset("assets/images/doctor_pic2.png", height: 220),
+                SizedBox(
+                  width: 20,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - 222,
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Nguyen Xuan Mai Trinh",
+                        style: Theme.of(context).textTheme.headline6!.copyWith(
+                              fontSize: 25,
+                            ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.work, size: 20, color: Colors.grey),
+                          const SizedBox(width: 5),
+                          Text(
+                            'Teacher',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          IconTile(
+                            backColor: Color(0xffFFECDD),
+                            imgAssetPath: "assets/images/email.png",
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          IconTile(
+                            backColor: Color(0xffFEF2F0),
+                            imgAssetPath: "assets/images/call.png",
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 26,
+            ),
+            // Text(
+            //   "About",
+            //   style: TextStyle(fontSize: 22),
+            // ),
+            // SizedBox(
+            //   height: 16,
+            // ),
+            // Text(
+            //   "I love pet. I believe that they're deserve to have a better life !!!",
+            //   style: TextStyle(color: Colors.grey, fontSize: 16),
+            // ),
+            // SizedBox(
+            //   height: 24,
+            // ),
+            Row(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Image.asset("assets/images/mappin.png"),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Address",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6!
+                                  .copyWith(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 18,
+                                  ),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width - 268,
+                              child: Text(
+                                "144 Duong Dinh Hoi, Thu Duc,\n Ho Chi Minh.",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Image.asset("assets/images/clock.png"),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "Available time",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline6!
+                                  .copyWith(
+                                    color: Colors.black87.withOpacity(0.7),
+                                    fontSize: 18,
+                                  ),
+                            ),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width - 268,
+                              child: Text(
+                                'Monday - Friday\n 17h30 - 23h',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .headline5!
+                                    .copyWith(color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Image.asset(
+                  "assets/images/map.png",
+                  width: 180,
+                )
+              ],
+            ),
+            Text(
+              "Activity",
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Color(0xff242424),
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),
+            ),
+            SizedBox(
+              height: 22,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 28, horizontal: 16),
+                    decoration: BoxDecoration(
+                        color: Color(0xffFBB97C),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Color(0xffFCCA9B),
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Image.asset("assets/images/list.png")),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2 - 130,
+                          child: Text(
+                            "List Of report",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5!
+                                .copyWith(color: Colors.white, fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                    decoration: BoxDecoration(
+                        color: Color(0xffA5A5A5),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                                color: Color(0xffBBBBBB),
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Image.asset("assets/images/list.png")),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 2 - 130,
+                          child: Text(
+                            "List of rescued pets",
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5!
+                                .copyWith(color: Colors.white, fontSize: 16),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+
+            Container(
+              constraints: BoxConstraints(maxWidth: 340.0, minHeight: 50.0),
+              margin: EdgeInsets.all(10),
+              child: RaisedButton(
+                onPressed: () {
+                  Get.to(EditProfile());
+                },
+                splashColor: Colors.blue.shade300,
+                color: ColorConstants.primary.withOpacity(0.8),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Edit Profile',
+                      style: Theme.of(context).textTheme.headline5!.copyWith(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                    ),
+                    Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class IconTile extends StatelessWidget {
+  final String imgAssetPath;
+  final Color backColor;
+
+  IconTile({required this.imgAssetPath, required this.backColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 16),
+      child: Container(
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+            color: backColor, borderRadius: BorderRadius.circular(15)),
+        child: Image.asset(
+          imgAssetPath,
+          width: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/modules/me/volunteer_edit_profile.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class EditProfile extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    final double addHeight = 0.03;
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Edit profile',
+          style: Theme.of(context).textTheme.headline6!.copyWith(fontSize: 20),
+        ),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Container(
+        height: double.infinity,
+        padding: const EdgeInsets.only(left: 25, right: 25),
+        child: Form(
+          //key for form
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10)),
+                  child: GridTile(
+                    child: Image.asset(
+                      'assets/images/doctor_pic2.png',
+                      width: 200,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                TextFormField(
+                    autofocus: true,
+                    textInputAction: TextInputAction.done,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      alignLabelWithHint: true,
+                      labelText: "Full Name",
+                      hintText: "Nguyen Xuan Mai Trinh",
+                      labelStyle: Theme.of(context).textTheme.headline6,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb5b0ac)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF0A9396)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter your name";
+                      } else {
+                        return null;
+                      }
+                    }),
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                TextFormField(
+                    autofocus: true,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Phone",
+                      hintText: '0773 853 338',
+                      labelStyle: Theme.of(context).textTheme.headline6,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb5b0ac)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF0A9396)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter correct phone";
+                      } else {
+                        return null;
+                      }
+                    }),
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Address",
+                      hintText: '144 Duong Dinh Hoi, Thu Duc,\n Ho Chi Minh.',
+                      labelStyle: Theme.of(context).textTheme.headline6,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb5b0ac)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF0A9396)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter correct address";
+                      } else {
+                        return null;
+                      }
+                    }),
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                TextFormField(
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: "Teacher",
+                      hintText: 'Teacher',
+                      labelStyle: Theme.of(context).textTheme.headline6,
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFFb5b0ac)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF0A9396)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter your job";
+                      } else {
+                        return null;
+                      }
+                    }),
+                SizedBox(
+                  height: height * addHeight,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 100,
+                      height: 100,
+                      padding: const EdgeInsets.only(bottom: 50),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Get.back(result: true);
+                        },
+                        child: Text(
+                          'SAVE',
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all(
+                              Theme.of(context).primaryColor),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
